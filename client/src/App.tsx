d7b946c95e3dcdd28d5e286accd2644ae4bf19ff
patte <p@tte.io>
2024-02-01 22:50:16 +0300

    fix: cleanup colorMode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import type { Component } from "solid-js";
 import { ColorModeProvider } from "@kobalte/core";
 import Home from "./Home";
+import { ColorModeToggle } from "~/components/ColorModeToggle";
+import { cn } from "~/lib/utils";
 
 const App: Component = () => {
   return (
     <ColorModeProvider>
+      <div class={cn("flex items-end justify-end p-2")}>
+        <ColorModeToggle />
+      </div>
       <Home />
     </ColorModeProvider>
   );
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,13 +1,9 @@
 import { Card } from "~/components/ui/card";
 import { Flex } from "~/components/ui/flex";
-import { useColorMode } from "@kobalte/core";
 
 export default function Home() {
-  const { setColorMode } = useColorMode();
   return (
     <div>
-      <button onClick={() => setColorMode("dark")}>Dark</button>
-      <button onClick={() => setColorMode("light")}>Light</button>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
       <Card class="w-full max-w-xs p-6">
         <Flex>
