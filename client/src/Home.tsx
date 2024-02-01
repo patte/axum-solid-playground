3cdb71f28cfdee03c60f9b2d22fa4a8585ebf6e5
patte <p@tte.io>
2024-02-01 22:56:51 +0300

    fix: cleanup

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,26 +1,13 @@
 import { Card } from "~/components/ui/card";
-import { Flex } from "~/components/ui/flex";
 
 export default function Home() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <Card class="w-full max-w-xs p-6">
-        <Flex>
-          <div>
-            <p class="text-card-foreground/70 text-sm font-normal">
-              Tickets sold
-            </p>
-            <p class="text-card-foreground text-3xl font-semibold">9,876</p>
-          </div>
-          <div>
-            <p class="text-card-foreground/70 text-sm font-normal">
-              Average Selling Price
-            </p>
-            <p class="text-card-foreground text-3xl font-semibold">$ 175.20</p>
-          </div>
-        </Flex>
-      </Card>
+      <div class="flex items-center justify-center">
+        <Card class="w-full max-w-xs p-6 text-center">
+          <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        </Card>
+      </div>
     </div>
   );
 }
